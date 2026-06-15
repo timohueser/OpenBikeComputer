633dc32915faa459d21b8ead7399880aa85f79bb
timohueser <timo.hueser@gmail.com>
2026-06-15 17:42:34 +0200

    refactor: optimize OSM ingestor and add edge case tests

--- obcm/ingest.py
+++ obcm/ingest.py
@@ -8,23 +8,29 @@ class OSMHandler(osmium.SimpleHandler):
         self.features = []
 
     def way(self, w):
-        for key, values in self.config["features"].items():
-            if key in w.tags and w.tags[key] in values:
-                style = values[w.tags[key]]
-                try:
-                    # Extract coordinates from nodes
-                    coords = [(n.lon, n.lat) for n in w.nodes]
-                    if len(coords) < 2:
-                        return
-                    self.features.append({
-                        "style_id": style["id"],
-                        "geometry": LineString(coords)
-                    })
-                except osmium.InvalidLocationError:
-                    return
+        style = None
+        for tag_key, tag_val in w.tags:
+            if tag_key in self.config["features"] and tag_val in self.config["features"][tag_key]:
+                style = self.config["features"][tag_key][tag_val]
+                break
+        
+        if not style:
+            return
+
+        try:
+            coords = [(n.lon, n.lat) for n in w.nodes]
+            if len(coords) < 2:
+                return
+            self.features.append({
+                "style_id": style["id"],
+                "geometry": LineString(coords)
+            })
+        except osmium.InvalidLocationError:
+            return
 
 def ingest_osm(pbf_path, config):
     handler = OSMHandler(config)
     # Use locations=True to resolve node references to coordinates
-    handler.apply_file(pbf_path, locations=True)
+    # Use flex_mem for efficient node location indexing
+    handler.apply_file(pbf_path, locations=True, idx='flex_mem')
     return handler.features
